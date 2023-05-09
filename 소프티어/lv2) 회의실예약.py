7416ef9e3043a8ffa751642e8d858e1d2698a45f
parksanghyeop <48915018+parksanghyeop@users.noreply.github.com>
2023-05-09 14:50:27 +0900

    Create lv2) 회의실예약.py

---
+++ 소프티어/lv2) 회의실예약.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+def checkTime(times):
+    available = []
+    check = 9
+    for time in times:
+        start, end = time[0], time[1]
+        if (check < start):
+            available.append([check, start])
+        check = end
+
+    if(check != 18):
+        available.append([check,18])
+    
+    length = len(available)
+    if(length==0):
+        print("Not available")
+    else:
+        print(length, "available:")
+        for start, end in available:
+            if(start == 9): start = "09"
+            print(str(start)+"-"+str(end))
+                
+                
+n, m = map(int,input().split())
+reservations = dict()
+
+for _ in range(n):
+    room = input()
+    reservations[room[:-1]] = []
+
+for _ in range(m):
+    room, start, tend = map(str, input().split())
+    reservations[room].append([int(start), int(tend)])
+
+last_check = False
+
+
+for room, times in sorted(reservations.items()):
+    times.sort(key=lambda x: x[0])
+    
+    print("Room "+room+":")
+    checkTime(times)
+    
+    n-=1
+    if(n != 0): 
+        print("-----")
