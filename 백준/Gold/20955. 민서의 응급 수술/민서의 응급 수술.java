e5c3571bf1743c7248dd9bbed51b09372e2e2fc5
parksanghyeop <48915018+parksanghyeop@users.noreply.github.com>
2023-09-06 20:50:08 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 380 ms, Memory: 37848 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
@@ -1,49 +1,63 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-    static int[] parents;
 
-    static void init(int n) {
-        parents = new int[n + 1];
-        Arrays.fill(parents, -1);
-    }
+    static int[] parent;
 
-    static int find(int a) {
-        if (parents[a] < 0) return a;
-        return parents[a] = find(parents[a]);
-    }
-
-    static boolean union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a == b) return false;
-        int h = parents[a] < parents[b] ? a : b;
-        int l = parents[a] < parents[b] ? b : a;
-        parents[h] += parents[l];
-        parents[l] = h;
-        return true;
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        init(n);
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        Arrays.fill(parent, -1);
+//        parent = IntStream.rangeClosed(0,N).toArray();
 
         int count = 0;
-        while (m-- > 0) {
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            if (!union(u, v)) count++;
+            if (!union(u, v)) { // 이미 두개가 같은 집합이라면 사이클이 존재한는 뜻이므로 사이클을 제거하는 연산 +1
+                count++;
+            }
         }
-        for (int i = 1; i <= n; i++) {
-            if (parents[i] < 0) count++;
+        for (int i = 1; i < N + 1; i++) {
+            if (parent[i] < 0) {
+                count++;
+            }
         }
         System.out.println(--count);
+    }
+
+    private static boolean union(int a, int b) {
+//        System.out.println("=====================");
+//        System.out.println(a + " " + b);
+        a = find(a);
+        b = find(b);
+//        System.out.println("findA: " + a + " findB: " + b);
+        if (a == b) return false;
+        int h = parent[a] < parent[b] ? a : b;
+        int l = parent[a] < parent[b] ? b : a;
+
+        parent[h] += parent[l]; // 더 많은 자식을 가진 루트로 만들어줌
+        parent[l] = h;
+//        System.out.println("h : " + h + " | l : " + l);
+//        System.out.println(Arrays.toString(parent));
+        return true;
+    }
+
+    private static int find(int a) {
+        if (parent[a] < 0) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
     }
 }
